e3f457b231ba3b6b32d7b94b0b8701fec19bc4ee
kernelmachine <suching@fb.com>
2022-05-03 14:05:06 -0700

    update

--- slurm_jobs/example_mod_train_sweep.py
+++ slurm_jobs/example_mod_train_sweep.py
@@ -15,10 +15,10 @@ DRY_MODE = False
 name_keys = ["MODEL",  "PHASE_ONE_RATIO", "RESET_ITEMS", "LR", "UPDATE_FREQ", "DOMAIN_ID"]
 NUM_GPUS = 8
 NUM_NODES = 1
-CHECKPOINTS_TOP_FOLDER = '/gscratch/zlab/margsli/demix-checkpoints/models/suchin/'
-NEW_MODEL_TOP_FOLDER = '/gscratch/zlab/margsli/demix-checkpoints/models_test/suchin/'
-# CHECKPOINTS_TOP_FOLDER = '/checkpoint/suching/margaret_sweep_rerun/small/'
-# NEW_MODEL_TOP_FOLDER = '/checkpoint/suching/mod_sweep/_modular_gpt3_small_36K/modular_gpt3_small_36K_LR=0.001/'
+#CHECKPOINTS_TOP_FOLDER = '/gscratch/zlab/margsli/demix-checkpoints/models/suchin/'
+#NEW_MODEL_TOP_FOLDER = '/gscratch/zlab/margsli/demix-checkpoints/models_test/suchin/'
+CHECKPOINTS_TOP_FOLDER = '/checkpoint/suching/margaret_sweep_rerun/small/'
+NEW_MODEL_TOP_FOLDER = '/checkpoint/suching/mod_sweep/_modular_gpt3_small_36K/modular_gpt3_small_36K_LR=0.001/'
 
 re_string = ''
 FOLDERS = mod_checkpoint_utils.find_folders(CHECKPOINTS_TOP_FOLDER, re_string=re_string)
@@ -36,11 +36,11 @@ grids = {
             "COPYING_MODEL_FOLDER": [CHECKPOINTS_TOP_FOLDER],
             "NEW_MODEL_TOP_FOLDER": [NEW_MODEL_TOP_FOLDER],
             "CHECKPOINTS_SUBFOLDER": FOLDERS,
-            "PHASE_ONE_RATIO": [0.25, 0.5, 0.75],
-            "RESET_ITEMS": ['dataloader,lr-scheduler,meters,optimizer'],
+            "PHASE_ONE_RATIO": [0.5],
+            "RESET_ITEMS": ['dataloader,optimizer'],
             "NUM_STEPS": [36000],
-            "UPDATE_FREQ": [4],
-            "LR": [1e-4],
+            "UPDATE_FREQ": [32],
+            "LR": [5e-4,1e-3,2e-3],
             "WANDB_PROJECT": ['test'],
             "WANDB_ENTITY": ['scaling-demix'],
             "MOD_FOLDER": [MOD_FOLDER],
