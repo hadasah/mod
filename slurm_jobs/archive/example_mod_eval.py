2b1f0f6f9e1cfdf5e0f90b29ea9eb491d181642b
kernelmachine <suching@fb.com>
2022-05-23 13:51:15 -0700

    update

--- slurm_jobs/archive/example_mod_eval.py
+++ slurm_jobs/archive/example_mod_eval.py
@@ -8,34 +8,32 @@ if username not in CONSTANTS:
     raise Error("username isn't defined in slurm_constants file")
 RUN_CONSTANTS = CONSTANTS.get(username)
 MOD_FOLDER = RUN_CONSTANTS.get('MOD_FOLDER')
-MODEL_FOLDER = "/checkpoint/suching/mod/"
+MODEL_FOLDER = RUN_CONSTANTS.get('MODEL_FOLDER')
 DATA_BIN = RUN_CONSTANTS.get('DATA_BIN')
 JQ_PATH = RUN_CONSTANTS.get('JQ_PATH')
 
-SWEEP_NAME = "eval_sweep_transformer_lm_gpt3_medium_mod_LOAD_FROM_STEP_0"
+SWEEP_NAME = "eval_sweep_gpt3_small_mod_MOD_STEPS_FROM_SCRATCH"
 DEBUG_MODE = False
 DRY_MODE = False
 name_keys = []
 NUM_GPUS = 8
 
 # /checkpoint/suching/suchin_mod_8_GPUs/small/_EXPERIMENT=dense_NUMSTEPS=36000_LR=0.001/_DOMAIN_5_MOD_STEPS_30000_PHASE1_DENSE
-# /checkpoint/suching/mod_sweep/_modular_gpt3_small_36K/modular_gpt3_small_36K_LR=0.001/MODEL=transformerlmgpt3small_DOMAINID=7_PHASEONERATIO=0.25_RESETITEMS=dataloader,meters_UPDATEFREQ=32_LR=0.001
-
-# MODEL=transformerlmgpt3small_DOMAINID=7_PHASEONERATIO=0.25_RESETITEMS=dataloader,meters_UPDATEFREQ=32_LR=0.001
 
 # This regex looks in MODEL_FOLDER's subfolders for matches
 WANTED_FOLDER_REGEX = '.*'
 # Used to distinguish between my naming conventions for demix vs modular models
 MODEL_TYPE = 'modular'
 # Determines where the posteriors and results gets saved 
-EVAL_FOLDER_ID = 'Base_dense_LOAD_FROM_STEP_0_LR_0.0005'
+EVAL_FOLDER_ID = 'Base_dense_MOD_STEPS_PHASE_RATIO_MOD_STEPS_FROM_SCRATCH'
 # Comma separated list of the checkpoint IDs. 
 #Unfortunately this can't be set per job, I'm assuming we're always setting the right # updates
 CHECKPOINT_IDS = 'best,best,best,best,best,best,best,best'
 EVAL_SCRIPT = f'{MOD_FOLDER}/demix/mix_eval_pipeline.sh' if MODEL_TYPE in ['demix', 'modular'] else f'{MOD_FOLDER}/demix/eval_pipeline.sh'
-all_runs = os.listdir("/checkpoint/suching/mod/_modular_transformer_lm_gpt3_medium/modular_transformer_lm_gpt3_medium_LR=0.0005/")
+all_runs = os.listdir(MODEL_FOLDER + "/mod/")
 regex = re.compile(WANTED_FOLDER_REGEX)
-selected_folders = [folder for folder in all_runs if regex.match(folder)]
+print(all_runs)
+selected_folders = [folder for folder in all_runs if regex.match(folder) and "small" in folder]
 print(selected_folders)
 grids = {
     SWEEP_NAME: {
@@ -43,8 +41,8 @@ grids = {
         'positional_args': {
             "NUM_GPUS": [NUM_GPUS],
             "DATA_BIN": [DATA_BIN],
-            "ROOT_MODEL_FOLDER": ["/checkpoint/suching/mod/_modular_transformer_lm_gpt3_medium/modular_transformer_lm_gpt3_medium_LR=0.0005/"],
-            "MODEL_FOLDER": ['.'],
+            "ROOT_MODEL_FOLDER": [MODEL_FOLDER + "/mod/"],
+            "MODEL_FOLDERS": selected_folders,
             "CHECKPOINT_IDS": [CHECKPOINT_IDS],
             "DOMAIN_ID": [i for i in range(16)],
             "ENSEMBLE_TYPE": ['cached_prior'],
@@ -53,7 +51,7 @@ grids = {
             "GENERALIST_MODEL": ["None"],
             "TOP_K": [8],
             "EVAL_FOLDER_ID": [EVAL_FOLDER_ID],
-            "LOAD_FROM_STEP": [0],
+            "NUM_STEPS": [40000],
             "EXCLUDE_EXPERT": ["False"],
             "ONLY_USE_DOMAIN_EXPERT": ['False'],
             "MOD_FOLDER": [MOD_FOLDER],
